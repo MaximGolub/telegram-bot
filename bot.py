ffe65c5f1f786e1fb59204b88d4e776e7c6f8e4c
MaximGolub <33345242+MaximGolub@users.noreply.github.com>
2025-05-28 19:46:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,13 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
-# Обработка команды /start
+TOKEN = os.environ.get("7554030982:AAEtmxssa5thfvKPMR-7y3agTwAwYHCarSg")
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я твой Telegram-бот.")
+    await update.message.reply_text("Привет с Render!")
 
-# Ответ на любое текстовое сообщение
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Ты сказал: {update.message.text}")
-
-# Основной блок
 if __name__ == '__main__':
-    app = ApplicationBuilder().token("7554030982:AAEtmxssa5thfvKPMR-7y3agTwAwYHCarSg").build()
-
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    print("Бот запущен. Нажми Ctrl+C для остановки.")
     app.run_polling()
